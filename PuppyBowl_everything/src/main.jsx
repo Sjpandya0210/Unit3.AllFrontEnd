18a144842229663f614e804aabe60e426c8edfce
Sjpandya0210 <Sjpandya1991@gmail.com>
2024-02-26 19:00:44 -0500

    name changed

--- PuppyBowl_everything/src/main.jsx
+++ PuppyBowl_everything/src/main.jsx
@@ -4,7 +4,7 @@ import {Provider} from "react-redux"
 import App from './App.jsx'
 import './index.css'
 //redux
-import store from '../Redux/store.js';
+import store from '../redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
